fa4defcab0c2bb1acae07fe3aff458ea1192e859
Bard Harald Rossehaug <br@makingview.com>
2020-02-02 00:46:45 +0100

    fuck

---
+++ GGJ-2020/Assets/B_RandomDeathSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_RandomDeathSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<AudioSource>().clip = _clips[Random.Range(0, _clips.Length - 1)];
+        this.GetComponent<AudioSource>().Play();
+    }
+}
