62ec1b6804750e1ce7a33a38ba794b3b3107078f
Bard Harald Rossehaug <br@makingview.com>
2020-02-01 14:45:59 +0100

    Fuck you all

---
+++ GGJ-2020/Assets/B_Stuff/B_Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_Door : MonoBehaviour
+{
+    [SerializeField] private Vector3 _openRotation = new Vector3(0, 170, 0);
+    [SerializeField] private float _speed = 50f;
+
+    private Vector3 _targetRotation;
+    private Vector3 _startRotation;
+
+    private void Update()
+    {
+        if(Quaternion.Angle(this.transform.rotation, Quaternion.Euler(_targetRotation)) > 0.001f)
+        {
+            // Target not met. Rotate more!
+            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.Euler(_targetRotation), Time.deltaTime * _speed);
+        }
+
+        if (!PlayerIsCloseEnough)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _targetRotation = _targetRotation == _openRotation ? _startRotation : _openRotation;
+        }
+    }
+
+    public bool PlayerIsCloseEnough { private get; set; } = false;
+}
