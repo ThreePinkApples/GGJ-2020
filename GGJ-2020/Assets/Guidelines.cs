15ce5f1f7e2e80eeab2781c38f49cd71591f4ed0
Lars <hagen.lars.s@gmail.com>
2020-02-01 11:18:57 +0100

    Opprettet egen folderstruktur
    Opprettet egen folderstruktur + litt mer

---
+++ GGJ-2020/Assets/Guidelines.cs
@@ -0,0 +1,46 @@
+﻿/* GGJ 2020
+ * 
+ * - Guidelines for oss selv
+ * 
+ * - Alle må navngi scripts og assets med initialen foran i navnet, feks. L_MasterClass.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
