41bce0265e125c54d560cb8d74c6e0eb0f1a2da1
Lars <hagen.lars.s@gmail.com>
2020-02-02 01:29:49 +0100

    fisk

--- GGJ-2020/Assets/Lemons Stuff - do not touch/Scripts/L_ScriptDamnGoodBeanJuice.cs
+++ GGJ-2020/Assets/Lemons Stuff - do not touch/Scripts/L_ScriptDamnGoodBeanJuice.cs
@@ -21,7 +21,6 @@ public class L_ScriptDamnGoodBeanJuice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DrinkTheBeanJuice();
 
         if (drink)
         {
